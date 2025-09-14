eb42b71f2ddf151b678832ac54fbc23b3876066e
Tiago-Code-Dev <tiagodevcodeu@gmail.com>
2025-09-14 02:20:09 -0300

    refactor(proposal): clean Program.cs using modular Extensions for authentication, swagger, validation, http resilience and middleware

---
+++ src/ProposalService/ProposalService.Api/Extensions/ConfigureMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using ProposalService.Api.Extensions;
+using Shared.CrossCutting.Middleware;
+
+public static class ApplicationExtensions
+{
+    public static IApplicationBuilder ConfigureMiddleware(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwaggerDocumentation(env);
+        }
+
+        app.UseHttpsRedirection();
+        app.UseJwtAuthenticationConfig();
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+
+        return app;
+    }
+
+    public static IEndpointRouteBuilder ConfigureEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapControllers();
+        app.MapHealthChecks("/health");
+        return app;
+    }
+}
---
+++ src/ProposalService/ProposalService.Api/Extensions/ValidationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using ProposalService.Api.Validation;
+using ProposalService.Application.Validators;
+
+namespace ProposalService.Api.Extensions;
+
+public static class ValidationExtensions
+{
+    public static IServiceCollection AddValidationConfig(this IServiceCollection services)
+    {
+        services.AddFluentValidationAutoValidation();
+        services.AddValidatorsFromAssemblyContaining<CustomerDtoValidator>();
+        services.AddValidatorsFromAssemblyContaining<CreateProposalRequestValidator>();
+
+        return services;
+    }
+}
