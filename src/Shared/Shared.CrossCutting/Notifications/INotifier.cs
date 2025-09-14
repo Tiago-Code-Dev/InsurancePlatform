9852b8fa0de463c6224007d6a172b7b12aba9040
Tiago-Code-Dev <tiagodevcodeu@gmail.com>
2025-09-14 08:59:30 -0300

    feat(shared): add notification pattern for domain/application error handling

---
+++ src/Shared/Shared.CrossCutting/Notifications/INotifier.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.CrossCutting.Notifications
+{
+    public interface INotifier
+    {
+        bool HasNotification();
+        List<Notification> GetNotifications();
+        void Handle(Notification notification);
+    }
+}
---
+++ src/Shared/Shared.CrossCutting/Notifications/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.CrossCutting.Notifications;
+
+public class Notification
+{
+    public string Message { get; }
+
+    public Notification(string message)
+    {
+        Message = message;
+    }
+}
