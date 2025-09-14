3817255b140e0f7b394500e2c7e4ab9d6b547361
Tiago-Code-Dev <tiagodevcodeu@gmail.com>
2025-09-14 06:35:51 -0300

    feat(proposal-infra): add EF Core initial migration and owned-type mappings

---
+++ src/ProposalService/ProposalService.Infrastructure/Mappings/CustomerMap .cs
@@ -0,0 +1,31 @@
+﻿namespace ProposalService.Infrastructure.Mappings;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProposalService.Domain.Entities;
+
+public class CustomerMap : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.OwnsOne(c => c.Document, doc =>
+        {
+            doc.Property(d => d.Number)
+                .HasColumnName("DocumentNumber")
+                .IsRequired();
+        });
+
+        builder.OwnsOne(c => c.Email, email =>
+        {
+            email.Property(e => e.Address)
+                .HasColumnName("EmailAddress")
+                .IsRequired();
+        });
+    }
+}
