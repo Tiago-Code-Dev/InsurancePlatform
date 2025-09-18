bd1c501db5b778587ff350ecd47cdc7a5e199dfb
Tiago-Code-Dev <tiagodevcodeu@gmail.com>
2025-09-17 21:05:34 -0300

    feat: Reafact structure

--- src/AuthenticationService/AuthenticationService.Api/Controllers/WeatherForecastController.cs
+++ src/AuthenticationService/AuthenticationService.Api/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Authentication.Api.Controllers;
+namespace AuthenticationService.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- src/AuthenticationService/AuthenticationService.Api/WeatherForecast.cs
+++ src/AuthenticationService/AuthenticationService.Api/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Authentication.Api;
+namespace AuthenticationService.Api;
 
 public class WeatherForecast
 {
